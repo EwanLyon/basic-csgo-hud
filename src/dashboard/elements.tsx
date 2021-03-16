2eddd44ed279308340c0153ff86f246b41d2ed50
Ewan Lyon <clubwhomc@gmail.com>
2021-03-16 20:35:50 +1100

    Cleaned up file

--- src/dashboard/elements.tsx
+++ src/dashboard/elements.tsx
@@ -3,39 +3,18 @@ import { render } from 'react-dom';
 
 // React imports
 import { theme } from './theme';
-import { Button, ButtonGroup, withStyles, Theme, Grid } from '@material-ui/core';
-import { green, red } from '@material-ui/core/colors';
+import { ButtonGroup, Grid } from '@material-ui/core';
+import { GreenButton, RedButton } from './styled-ui';
 import { ThemeProvider } from '@material-ui/styles';
 
-// Interfaces
-
-const ShowButton = withStyles((theme: Theme) => ({
-	root: {
-		color: theme.palette.getContrastText(green[600]),
-		backgroundColor: green[600],
-		'&:hover': {
-			backgroundColor: green[400],
-		},
-	},
-}))(Button);
-
-const HideButton = withStyles((theme: Theme) => ({
-	root: {
-		color: theme.palette.getContrastText(red[600]),
-		backgroundColor: red[600],
-		'&:hover': {
-			backgroundColor: red[400],
-		},
-	},
-}))(Button);
 
 export const Elements: React.FunctionComponent = () => {
-	function showElement(elementName: string): void {
+	function showElement(elementName: string) {
 		console.log('Showing ' + elementName);
 		nodecg.sendMessage('basicShow', elementName);
 	}
 
-	function hideElement(elementName: string): void {
+	function hideElement(elementName: string) {
 		console.log('Hiding ' + elementName);
 		nodecg.sendMessage('basicHide', elementName);
 	}
@@ -44,16 +23,16 @@ export const Elements: React.FunctionComponent = () => {
 		<ThemeProvider theme={theme}>
 			<Grid container>
 				<ButtonGroup fullWidth style={{ marginBottom: 8 }}>
-					<ShowButton onClick={(): void => showElement('teamEco')}>Team Eco</ShowButton>
-					<HideButton onClick={(): void => hideElement('teamEco')}>Team Eco</HideButton>
+					<GreenButton variant="contained" onClick={() => showElement('teamEco')}>Team Eco</GreenButton>
+					<RedButton variant="contained" onClick={() => hideElement('teamEco')}>Team Eco</RedButton>
 				</ButtonGroup>
 				<ButtonGroup fullWidth>
-					<ShowButton onClick={(): void => showElement('teamNades')}>
+					<GreenButton variant="contained" onClick={() => showElement('teamNades')}>
 						Team Nades
-					</ShowButton>
-					<HideButton onClick={(): void => hideElement('teamNades')}>
+					</GreenButton>
+					<RedButton variant="contained" onClick={() => hideElement('teamNades')}>
 						Team Nades
-					</HideButton>
+					</RedButton>
 				</ButtonGroup>
 			</Grid>
 		</ThemeProvider>
